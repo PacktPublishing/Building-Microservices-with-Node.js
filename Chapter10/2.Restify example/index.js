8834dc9523dd558b0d1d6db7ffe10e5c3d8db816
Daniel <daniel.kapexhiu@gmail.com>
2024-02-29 21:42:31 +0100

    Add files via upload

--- Chapter10/2.Restify example/index.js
+++ Chapter10/2.Restify example/index.js
@@ -1,27 +1,23 @@
-// Import the restify module 
+// Import the restify module
 
-const restify = require('restify');  
+const restify = require('restify');
 
-// Create a server instance 
+// Create a server instance
 
-const server = restify.createServer();  
+const server = restify.createServer();
 
-// Define a GET handler for the /api/resource endpoint 
+// Define a GET handler for the /api/resource endpoint
 
-server.get('/api/resource', (req, res) => {  
+server.get('/api/resource', (req, res, next) => {
+  // Send a response with the text "Hello, API!"
 
-  // Send a response with the text "Hello, API!" 
+  res.send('Hello, API!');
+});
 
-  res.send('Hello, API!');  
+// Start the server and listen on port 3000
 
-});  
+server.listen(3000, () => {
+  // Log a message to the console
 
-// Start the server and listen on port 3000 
-
-server.listen(3000, () => {  
-
-  // Log a message to the console 
-
-  console.log('Server is running on port 3000');  
-
-}); 
+  console.log('Server is running on port 3000');
+});
